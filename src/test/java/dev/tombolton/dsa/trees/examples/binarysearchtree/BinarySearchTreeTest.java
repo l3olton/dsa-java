efc8896651dca0884cbbc11fa1846d5cebbfa6b6
l3olton <tbolton96@icloud.com>
2026-01-01 20:45:16 +0000

    simplify binary search tree tests

--- src/test/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTreeTest.java
+++ src/test/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTreeTest.java
@@ -1,28 +1,22 @@
 package dev.tombolton.dsa.trees.examples.binarysearchtree;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.Test;
+
 public class BinarySearchTreeTest {
+
     @Test
     public void searchShouldReturnCorrectNode() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
         /*
          *                   50
          *                  /  \
@@ -31,8 +25,10 @@ public class BinarySearchTreeTest {
          *               6  48 53 84
          */
 
+        var node48 = rootNode.getLeftChild().getRightChild();
+
         // Act
-        TreeNode<Integer> result = BinarySearchTree.search(48, root);
+        TreeNode<Integer> result = BinarySearchTree.search(48, rootNode);
 
         // Assert
         assertEquals(node48, result);
@@ -41,20 +37,13 @@ public class BinarySearchTreeTest {
     @Test
     public void insertShouldInsertNodeCorrectlyInTree() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
         /*
          *                   50
          *                  /  \
@@ -63,8 +52,10 @@ public class BinarySearchTreeTest {
          *               6  48 53 84
          */
 
+        var node53 = rootNode.getRightChild().getLeftChild();
+
         // Act
-        BinarySearchTree.insert(60, root);
+        BinarySearchTree.insert(60, rootNode);
         /*
          *                   50
          *                  /  \
@@ -114,20 +105,13 @@ public class BinarySearchTreeTest {
     @Test
     public void deleteShouldDeleteNodeFromTree_noChildren() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
         /*
          *                   50
          *                  /  \
@@ -136,8 +120,10 @@ public class BinarySearchTreeTest {
          *               6  48 53 84
          */
 
+        var node27 = rootNode.getLeftChild();
+
         // Act
-        BinarySearchTree.delete(6, root);
+        BinarySearchTree.delete(6, rootNode);
         /*
          *                   50
          *                  /  \
@@ -153,26 +139,35 @@ public class BinarySearchTreeTest {
     @Test
     public void deleteShouldDeleteNodeFromTree_leftChild() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-        TreeNode<Integer> node4 = new TreeNode<>(4);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
+        BinarySearchTree.insert(4, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               6  48 53 84
+         *              /
+         *             4
+         */
 
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        node6.setLeftChild(node4);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        var node27 = rootNode.getLeftChild();
 
         // Act
-        BinarySearchTree.delete(6, root);
+        BinarySearchTree.delete(6, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               4  48 53 84
+         */
 
         // Assert
         assertEquals(4, node27.getLeftChild().getValue());
@@ -181,26 +176,35 @@ public class BinarySearchTreeTest {
     @Test
     public void deleteShouldDeleteNodeFromTree_rightChild() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-        TreeNode<Integer> node8 = new TreeNode<>(8);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
+        BinarySearchTree.insert(8, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               6  48 53 84
+         *                \
+         *                 8
+         */
 
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        node6.setRightChild(node8);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        var node27 = rootNode.getLeftChild();
 
         // Act
-        BinarySearchTree.delete(6, root);
+        BinarySearchTree.delete(6, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               8  48 53 84
+         */
 
         // Assert
         assertEquals(8, node27.getLeftChild().getValue());
@@ -209,94 +213,121 @@ public class BinarySearchTreeTest {
     @Test
     public void deleteShouldDeleteNodeFromTree_twoChildren_leftSide() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-        TreeNode<Integer> node4 = new TreeNode<>(4);
-        TreeNode<Integer> node8 = new TreeNode<>(8);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
+        BinarySearchTree.insert(4, rootNode);
+        BinarySearchTree.insert(8, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               6  48 53 84
+         *              / \
+         *             4   8
+         */
 
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        node6.setLeftChild(node4);
-        node6.setRightChild(node8);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        var node27 = rootNode.getLeftChild();
 
         // Act
-        BinarySearchTree.delete(6, root);
+        BinarySearchTree.delete(6, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               8  48 53 84
+         *              /
+         *             4
+         */
 
         // Assert
-        assertEquals(8, node27.getLeftChild().getValue());
-        assertNull(node8.getLeftChild());
+        assertEquals(8, rootNode.getLeftChild().getLeftChild().getValue());
+        assertEquals(4, rootNode.getLeftChild().getLeftChild().getLeftChild().getValue());
+        assertNull(rootNode.getLeftChild().getLeftChild().getRightChild());
     }
 
     @Test
     public void deleteShouldDeleteNodeFromTree_twoChildren_rightSide() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-        TreeNode<Integer> node4 = new TreeNode<>(4);
-        TreeNode<Integer> node8 = new TreeNode<>(8);
-
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        node6.setLeftChild(node4);
-        node6.setRightChild(node8);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
+        BinarySearchTree.insert(4, rootNode);
+        BinarySearchTree.insert(8, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               6  48 53 84
+         *              / \
+         *             4   8
+         */
 
         // Act
-        BinarySearchTree.delete(72, root);
+        BinarySearchTree.delete(72, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   84
+         *                / \   /
+         *               6  48 53
+         *              / \
+         *             4   8
+         */
 
         // Assert
-        assertEquals(84, root.getRightChild().getValue());
-        assertEquals(53, root.getRightChild().getLeftChild().getValue());
-        assertNull(root.getRightChild().getRightChild());
+        assertEquals(84, rootNode.getRightChild().getValue());
+        assertEquals(53, rootNode.getRightChild().getLeftChild().getValue());
+        assertNull(rootNode.getRightChild().getRightChild());
     }
 
     @Test
     public void deleteShouldDeleteNodeFromTree_rootNode() {
         // Arrange
-        TreeNode<Integer> node72 = new TreeNode<>(72);
-        TreeNode<Integer> node48 = new TreeNode<>(48);
-        TreeNode<Integer> node84 = new TreeNode<>(84);
-        TreeNode<Integer> node53 = new TreeNode<>(53);
-        TreeNode<Integer> node27 = new TreeNode<>(27);
-        TreeNode<Integer> node6 = new TreeNode<>(6);
-        TreeNode<Integer> node4 = new TreeNode<>(4);
-        TreeNode<Integer> node8 = new TreeNode<>(8);
-
-        node72.setRightChild(node84);
-        node72.setLeftChild(node53);
-
-        node27.setRightChild(node48);
-        node27.setLeftChild(node6);
-
-        node6.setLeftChild(node4);
-        node6.setRightChild(node8);
-
-        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+        TreeNode<Integer> rootNode = new TreeNode<>(50);
+        BinarySearchTree.insert(27, rootNode);
+        BinarySearchTree.insert(72, rootNode);
+        BinarySearchTree.insert(48, rootNode);
+        BinarySearchTree.insert(6, rootNode);
+        BinarySearchTree.insert(84, rootNode);
+        BinarySearchTree.insert(53, rootNode);
+        BinarySearchTree.insert(4, rootNode);
+        BinarySearchTree.insert(8, rootNode);
+        /*
+         *                   50
+         *                  /  \
+         *                 27   72
+         *                / \   / \
+         *               6  48 53 84
+         *              / \
+         *             4   8
+         */
 
         // Act
-        BinarySearchTree.delete(50, root);
+        BinarySearchTree.delete(50, rootNode);
+        /*
+         *                   53
+         *                  /  \
+         *                 27   72
+         *                / \     \
+         *               6  48    84
+         *              / \
+         *             4   8
+         */
 
         // Assert
-        assertEquals(53, root.getValue());
-        assertNull(node72.getLeftChild());
+        assertEquals(53, rootNode.getValue());
+        assertNull(rootNode.getRightChild().getLeftChild());
     }
 }
